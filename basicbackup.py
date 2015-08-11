4800c6f3d880f6cdfdfe43ef53758260da204ad4
BMJHayward <brendanhayward85@gmail.com>
2015-08-11 13:48:02 +1000

    add python backup file

---
+++ basicbackup.py
@@ -0,0 +1,30 @@
+import datetime
+import os
+import shutil
+
+GOOGLE_DRIVE_DIRECTORY = 'C:/Users/username/Google Drive/'
+MAIN_BACKUP_DIRECTORY = 'C:/Users/username/Desktop/_Backups/md_backup_{0}'
+EXTERNAL_DRIVE_DIRECTORY = 'F:/My Files/_Backups/md_backup_{0}'
+
+def get_backup_directory(base_directory):
+    date = str(datetime.datetime.now())[:16]
+    date = date.replace(' ', '_').replace(':', '')
+    return base_directory.format(date)
+
+def copy_files(directory):
+    for file in os.listdir(GOOGLE_DRIVE_DIRECTORY):
+        file_path = os.path.join(GOOGLE_DRIVE_DIRECTORY, file)
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, directory)
+
+def perform_backup(base_directory):
+    backup_directory = get_backup_directory(base_directory)
+    os.makedirs(backup_directory)
+    copy_files(backup_directory)
+
+def main():
+    perform_backup(MAIN_BACKUP_DIRECTORY)
+    perform_backup(EXTERNAL_DRIVE_DIRECTORY)
+
+if __name__ == '__main__':
+    main()
