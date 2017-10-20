9c86e57b78ac590a121c199917c433d18aa47c93
Brendan Hayward <brendanhayward85@gmail.com>
2017-10-20 15:06:17 +1100

    fix indices, add run_LCS function

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 def insertion_sort(array, incrs=True):
     for j in range(1, len(array)):
         key = array[j]
@@ -94,48 +95,55 @@ def longest_common_subsequence(X,Y):
     b = [[i for i in range(1,m)],[j for j in range(1,n)]]
     c = [[i for i in range(m)],[j for j in range(n)]] 
     for i in range(1,m): 
-        c[i,0] = 0
+        c[i][0] = 0
     for j in range(n):
-        c[0,j] = 0
+        c[0][j] = 0
     for i in range(1,m):
         for j in range(1,n):
             if X[i] == Y[j]:
-                c[i,j] = c[i-1,j-1] + 1
-                b[i,j] = 'up left'
-            elif c[i-1,j] >= c[i,j-1]:
-                c[i,j] = c[i-1,j]
-                b[i,j] = 'up'
+                c[i][j] = c[i-1][j-1] + 1
+                b[i][j] = 'up left'
+            elif c[i-1][j] >= c[i][j-1]:
+                c[i][j] = c[i-1,j]
+                b[i][j] = 'up'
             else:
-                c[i,j] = c[i,j-1]
-                b[i,j] = 'left'
+                c[i][j] = c[i][j-1]
+                b[i][j] = 'left'
     return c, b
 
 def print_LCS(b,X,i,j):
     if i==0 | j==0:
         return
-    if b[i,j] == 'up left':
+    if b[i][j] == 'up left':
         print_LCS(b,X,i-1,j-1)
         print(X[i])
-    elif b[i,j] == 'up':
+    elif b[i][j] == 'up':
         print_LCS(b,X,i-1,j)
     else:
         print_LCS(b,X,i,j-1)
+
+def run_LCS():
+    dna = {0:'a', 1:'c', 2:'g', 3:'t'}
+    X = [dna[np.random.randint(4)] for i in range(20)]
+    Y = [dna[np.random.randint(4)] for i in range(20)]
+    commons, path = longest_common_subsequence(X,Y)
+    print_LCS(path,X,len(X),len(commons))
 
 def optimal_BST(p,q,n):
     e = [[i for i in range(1,n+1)],[j for j in range(n)]]
     w = [[i for i in range(1,n+1)],[j for j in range(n)]]
     root = [[i for i in range(1,n)],[j for j in range(1,n)]]
     for i in range(1,n+1):
-        e[i,i-1] = q[i-1]
-        w[i,i-1] = q[i-1]
+        e[i][i-1] = q[i-1]
+        w[i][i-1] = q[i-1]
     for l in range(1,n):
         for i in range(1,n-l+1):
             j = i+l-1
-            e[i,j] = math.inf
-            w[i,j] = w[i,j-1] + p[j] + q[j]
+            e[i][j] = math.inf
+            w[i][j] = w[i][j-1] + p[j] + q[j]
             for r in range(i,j):
-                t = e[i,r-1] + e[r+1,j] + w[i,j]
-                if t < e[i,j]:
-                    e[i,j] = t
-                    root[i,j] = r
+                t = e[i][r-1] + e[r+1][j] + w[i][j]
+                if t < e[i][j]:
+                    e[i][j] = t
+                    root[i][j] = r
     return e, root
